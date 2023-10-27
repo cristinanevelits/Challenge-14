975e343ac876a8726026cd41bbae2f085e7cb48a
Cristina Nevelits <cristinanevelits@yahoo.com>
2023-10-27 17:46:03 +0300

    Challenge 14

--- config/database.js
+++ config/database.js
@@ -5,6 +5,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "mysql",
   protocol: "mysql",
   logging: true,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 export default sequelize;
